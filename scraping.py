6c1d0c76b1acbdfb9a79150d3d63fc934ea2943b
ryo <ryo@nakamuraniwabi-no-MacBook-Pro.local>
2017-10-10 04:55:27 +0900

    fix: 諸々みやすいように修正

--- scraping.py
+++ scraping.py
@@ -1,24 +1,33 @@
+# -*- coding: utf-8 -*-
+
 import requests
-url = "https://news.yahoo.co.jp/topics"
-response = requests.get(url)
-response.status_code
-
-from bs4 import BeautifulSoup
-bs = BeautifulSoup(response.content,"lxml")
-
-print(bs.select('a')[0])
-
-topics = bs.select('.fl, .fr')
-print(topics[0])
-
-print(topics[0].select('li')[0].text)
-
-news_topics = {}
-for news in topics:
-    topic = news.select('li')[0].text
-    news_topics[topic] = [news_topic.text for news_topic in news.select('li')[1:-2]]
-
 import pandas as pd
-topics_dt = pd.DataFrame.from_dict(news_topics)
-topics_dt.apply( lambda x: x.str.replace(r'(new|写真|動画)',''))
-#topics_dt.to_csv( "oppai.csv" )
+from bs4 import BeautifulSoup
+
+def scraping():
+    url = "https://news.yahoo.co.jp/topics"
+    response = requests.get(url)
+    response.status_code
+
+    bs = BeautifulSoup(response.content,"lxml")
+
+    print(bs.select('a')[0])
+
+    topics = bs.select('.fl, .fr')
+    print(topics[0])
+
+    print(topics[0].select('li')[0].text)
+
+    news_topics = {}
+    for news in topics:
+        topic = news.select('li')[0].text
+        news_topics[topic] = [news_topic.text for news_topic in news.select('li')[1:-2]]
+
+    topics_dt = pd.DataFrame.from_dict(news_topics)
+    topics_dt.apply( lambda x: x.str.replace(r'(new|写真|動画)',''))
+    #topics_dt.to_csv( "oppai.csv" )
+
+    return 0
+
+
+scraping()
